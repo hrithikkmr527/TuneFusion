271fc74016fb52bb121449e128315248e69c9cb7
Hrithik <hrithikkmr527@gmail.com>
2024-11-23 18:32:59 +0000

    init repo

---
+++ TuneFusion/Entities/Song.cs
@@ -0,0 +1,13 @@
+﻿namespace TuneFusion.Entities
+{
+    public class Song
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Duration { get; set; }
+
+        public string Artist { get; set; } 
+    }
+}
---
+++ TuneFusion/Entities/SongStem.cs
@@ -0,0 +1,13 @@
+﻿namespace TuneFusion.Entities
+{
+    public class SongStem
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Instrument { get; set; }
+
+        public decimal Duration { get; set; }
+    }
+}
